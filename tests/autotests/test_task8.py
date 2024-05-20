1da9c9222f0fd53230d16ee223e09ee88ada5b0a
Sergo Kovaltsov <sergeykovaltsov@gmail.com>
2024-05-20 05:57:16 +0300

    task 8

--- tests/autotests/test_task8.py
+++ tests/autotests/test_task8.py
@@ -15,12 +15,12 @@ from fixtures import graph
 
 # Fix import statements in try block to run tests
 try:
-    from project.task2 import graph_to_nfa, regex_to_dfa
-    from project.task3 import FiniteAutomaton
-    from project.task4 import reachability_with_constraints
-    from project.task7 import cfpq_with_matrix
-    from project.task6 import cfpq_with_hellings
-    from project.task8 import cfpq_with_tensor, cfg_to_rsm, ebnf_to_rsm
+    from project.task02 import graph_to_nfa, regex_to_dfa
+    from project.task03 import FiniteAutomaton
+    from project.task04 import reachability_with_constraints
+    from project.task07 import cfpq_with_matrix
+    from project.task06 import cfpq_with_hellings
+    from project.task08 import cfpq_with_tensor, cfg_to_rsm, ebnf_to_rsm
 except ImportError:
     pytestmark = pytest.mark.skip("Task 8 is not ready to test!")
 
